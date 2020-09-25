af7b0c52718627df77ecd7f20a30e653fdb886eb
noobmaster69 <cjaguirre1988@gmail.com>
2020-09-24 23:06:49 -0400

    While Loop test

---
+++ NestedDecision.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+
+public class NestedDecision {
+    public static void main(String[] args){
+
+        int testScore; //Numeric test score
+        String input;  // To hold the user's input
+
+        //Get the numeric test score
+        input = JOptionPane.showInputDialog("Enter your numeric " +
+                "test score and I will tell you the grade: ");
+
+        testScore = Integer.parseInt(input);
+
+        //Display the grade
+        if(testScore < 60)
+        {
+            JOptionPane.showMessageDialog(null, "Your grade is F");
+
+        }
+        else
+        {
+            if(testScore < 70)
+            {
+                JOptionPane.showMessageDialog(null, "Your grade is D.");
+            }
+            else
+            {
+                if(testScore < 80)
+                {
+                    JOptionPane.showMessageDialog(null, "Your grade is C.");
+
+                }
+                else
+                {
+                    if(testScore < 90)
+                    {
+                        JOptionPane.showMessageDialog(null, "Your grade is B.");
+                    }
+                    else
+                    {
+                        JOptionPane.showMessageDialog(null, "Your grade is A");
+                    }
+                }
+            }
+        }
+        System.exit(0);
+    }
+}
